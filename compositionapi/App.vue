50c7cb7832ddf6250dd565e48c5368e7c40de8d7
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-01-16 08:20:18 +0900

    fix

---
+++ compositionapi/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+  export default {
+    created() {
+      console.log('created')
+    },
+    mounted() {
+      console.log("mounted")
+    },
+    setup() {
+      console.log("setup")
+      return {
+        msg: "hello world"
+      }
+    }
+  }
+</script>
